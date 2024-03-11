6009ded9bd85559c76eefa33bfd692bbace50769
Valeria <markelovav896@gmail.com>
2024-03-11 23:50:55 +0200

    Lab1

---
+++ src/main/java/com/example/lab1_k/Lab1KApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1_k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1KApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1KApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1_k/Lab1KApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1KApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
